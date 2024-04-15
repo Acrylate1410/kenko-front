b48e33c7e06402ba5df1651b1fbd5e0746afb87f
Acrylate1410 <quangnm1410@gmail.com>
2024-04-15 09:12:51 +0700

    fhg

--- src/pages/ProductDetails.js
+++ src/pages/ProductDetails.js
@@ -52,7 +52,7 @@ export default function ProductDetails() {
               <div className="text-xs md:text-sm">Liên hệ để mua hàng</div>
             </Link>
         </div>
-        <div className="w-4/5 mx-auto mt-4  md:hidden ">
+        <div className="w-4/5 mx-auto mt-4 md:hidden">
             <div className="mt-2">
               <p className="mb-2"><span className="font-semibold">Trọng lượng: </span><span>{product.mass}</span></p>
               <p className="mb-2"><span className="font-semibold">Thành phần chính: </span><span>{product.ingredients && product.ingredients.replace("Thành phần chính: ", "")}</span></p>
@@ -73,37 +73,36 @@ export default function ProductDetails() {
 
 function Details(props) {
   const [tab, setTab] = useState(1)
-  const [hover, setHover] = useState(0)
   return (
       <div className="w-4/5 mx-auto my-8 text-justify">
         <div className="font-semibold my-4 md:my-6 py-2 md:flex border-y border-y-blue-950">
-          <div onClick={() => setTab(1)} onMouseEnter={() => setHover(1)} onMouseLeave={() => setHover(0)} className={tab === 1 || hover === 1 ? "px-7 bg-blue-950 text-white py-1.5 cursor-pointer rounded-lg mr-2 text-center mb-2 md:mb-0" : "px-7 py-1.5 cursor-pointer mr-2 text-center mb-2 md:mb-0"}>Mô tả sản phẩm</div>
-          <div onClick={() => setTab(2)} onMouseEnter={() => setHover(2)} onMouseLeave={() => setHover(0)} className={tab === 2 || hover === 2 ? "px-7 bg-blue-950 text-white py-1.5 cursor-pointer rounded-lg mr-2 text-center mb-2 md:mb-0" : "px-7 py-1.5 cursor-pointer mr-2 text-center mb-2 md:mb-0"}>Công dụng</div>
-          <div onClick={() => setTab(3)} onMouseEnter={() => setHover(3)} onMouseLeave={() => setHover(0)} className={tab === 3 || hover === 3 ? "px-7 bg-blue-950 text-white py-1.5 cursor-pointer rounded-lg mr-2 text-center mb-2 md:mb-0" : "px-7 py-1.5 cursor-pointer mr-2 text-center mb-2 md:mb-0"}>Hướng dẫn sử dụng</div>
+          <div onClick={() => setTab(1)} className={tab === 1 ? "px-7 bg-blue-950 text-white py-1.5 cursor-pointer rounded-lg mr-2 text-center mb-2 md:mb-0" : "px-7 py-1.5 cursor-pointer mr-2 text-center mb-2 md:mb-0 rounded-lg hover:bg-blue-950 hover:text-white"}>Mô tả sản phẩm</div>
+          <div onClick={() => setTab(2)} className={tab === 2 ? "px-7 bg-blue-950 text-white py-1.5 cursor-pointer rounded-lg mr-2 text-center mb-2 md:mb-0" : "px-7 py-1.5 cursor-pointer mr-2 text-center mb-2 md:mb-0 rounded-lg hover:bg-blue-950 hover:text-white"}>Công dụng</div>
+          <div onClick={() => setTab(3)} className={tab === 3 ? "px-7 bg-blue-950 text-white py-1.5 cursor-pointer rounded-lg mr-2 text-center mb-2 md:mb-0" : "px-7 py-1.5 cursor-pointer mr-2 text-center mb-2 md:mb-0 rounded-lg hover:bg-blue-950 hover:text-white"}>Hướng dẫn sử dụng</div>
         </div>
           {tab === 1 ? 
-            <div className="text-[15px] mt-2">
-                {props.product.description && props.product.description.map(i =>
-                  <div key={i}>{i}</div>  
-                )}
-            </div> : 
-          tab === 2 ?
-            <div className="text-[15px] mt-2">
-                {props.product.function && props.product.function.map(i =>
-                  <div className="flex items-start" key={i}>{"- " + i}</div>
-                )}
-            </div> :
-            <div className="text-[15px] mt-2">
-                <span className="font-semibold">Hướng dẫn sử dụng: </span>
-                {props.product.instruction && props.product.instruction.map(i =>
-                  <p key={i}>{"- " + i}</p> 
-                )}
-                <span className="font-semibold">Đối tượng: </span>
-                {props.product.subject && props.product.subject.map(i =>
-                  <p key={i}>{"- " + i}</p> 
-                )}  
-            </div>
-        }
+              <div className="text-[15px] mt-2">
+                  {props.product.description && props.product.description.map(i =>
+                    <div key={i}>{i}</div>  
+                  )}
+              </div> : 
+            tab === 2 ?
+              <div className="text-[15px] mt-2">
+                  {props.product.function && props.product.function.map(i =>
+                    <div className="flex items-start" key={i}>{"- " + i}</div>
+                  )}
+              </div> :
+              <div className="text-[15px] mt-2">
+                  <span className="font-semibold">Hướng dẫn sử dụng: </span>
+                  {props.product.instruction && props.product.instruction.map(i =>
+                    <p key={i}>{"- " + i}</p> 
+                  )}
+                  <span className="font-semibold">Đối tượng: </span>
+                  {props.product.subject && props.product.subject.map(i =>
+                    <p key={i}>{"- " + i}</p> 
+                  )}  
+              </div>
+          }
     </div>
   )
 }
@@ -130,7 +129,7 @@ function SwiperComponent(props) {
           </SwiperSlide>
         )}
       </Swiper>
-      <Swiper pagination={true} modules={[Pagination]} className="mySwiper  w-full aspect-square  shadow-[0_10px_10px_-10px_rgba(0,0,0,0.3)] md:!hidden">
+      <Swiper pagination={true} modules={[Pagination]} className="mySwiper w-full aspect-square  shadow-[0_10px_10px_-10px_rgba(0,0,0,0.3)] md:!hidden">
         {[props.slide1, props.slide2].map(i => 
           <SwiperSlide className="w-full !flex items-center justify-center h-full">
             <img src={i} className="h-[75%]" alt={props.name}/>
